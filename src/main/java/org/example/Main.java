22a0c4ddce44112676f05fc58cc75963f5c45a79
sumanth <sumanthinavolu@gmail.com>
2024-04-02 22:11:09 +0530

    SQL preparedStatement

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,60 @@
 package org.example;
-
+import java.sql.*;
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        StudentDao sd = new StudentDao();
+        sd.connect();
+        Student s = new Student();
+        s.rollno = 5;
+        s.sname = "sql";
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        sd.addStudent(s);
+        sd.close();
 
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
+    }
+}
+class StudentDao{
+    static Connection con = null;
+    static Statement st = null;
+    public static void connect(){
+        try{
+            String url = "jdbc:mysql://localhost:3306/demo";
+            String uname = "root";
+            String pwd = "x1x2x3x4";
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection(url, uname, pwd);
+        }
+        catch(Exception e){
+            System.out.println(e);
         }
     }
+    public static void addStudent(Student s){
+        try{
+            String query = "insert into student values (?,?)";
+            PreparedStatement ps = con.prepareStatement(query);
+            ps.setInt(1,s.rollno);
+            ps.setString(2,s.sname);
+            ps.executeUpdate();
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+        //return null;
+    }
+    public static void close(){
+        try{
+            con.close();
+            st.close();
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+    }
+}
+class Student{
+    int rollno;
+    String sname;
 }
